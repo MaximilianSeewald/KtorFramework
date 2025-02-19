5fbedda49b6f5ae257a075837bb3f8740ce48035
Seewald Maximilian <Maximilian-Seewald@gmx.net>
2025-02-19 03:04:16 +0100

    remove unused imports

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {FormsModule} from '@angular/forms';
-import {MatIcon} from '@angular/material/icon';
-import {MatIconButton} from '@angular/material/button';
-import {MatToolbar} from '@angular/material/toolbar';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, MatIcon, MatIconButton, MatToolbar, RouterLink],
+  imports: [RouterOutlet, FormsModule, RouterLink],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css',
