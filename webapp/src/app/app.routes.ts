99d136e09cf08b15a59a84142a2d77dfb5558997
Seewald Maximilian <Maximilian-Seewald@gmx.net>
2025-02-19 03:01:54 +0100

    add Landing page and new header and footer

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {AuthGuard, NoAuthGuard} from './auth.guard';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {LandingComponent} from './landing/landing.component';
 
 export const routes: Routes = [
-  {path: 'login', component: LoginComponent, canActivate: [NoAuthGuard]},
+  { path: 'login', component: LoginComponent, canActivate: [NoAuthGuard]},
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: '/login'}
+  { path: 'landing', component: LandingComponent },
+  { path: '**', redirectTo: '/landing'}
 ];
