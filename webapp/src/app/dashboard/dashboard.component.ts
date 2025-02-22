40782dd8d8dcc2b1c5d29efa96f8cf6157f07a70
Seewald Maximilian <Maximilian-Seewald@gmx.net>
2025-02-22 02:45:46 +0100

    add FrontEndDraft for ShoppingList

--- webapp/src/app/dashboard/dashboard.component.ts
+++ webapp/src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatCard, MatCardHeader} from '@angular/material/card';
 import {MatIcon} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {NgForOf, NgIf} from '@angular/common';
+import {ShoppingListItem} from '../shoppingList/shoppingList.model';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     MatCard,
     MatCardHeader,
-    MatIcon
+    MatIcon,
+    FormsModule,
+    NgIf,
+    NgForOf
   ],
   templateUrl: './dashboard.component.html',
   standalone: true,
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  apiUrl = environment.apiUrl;
+  shoppingList: ShoppingListItem[] = [];
+  newItemName = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getShoppingItems()
+  }
+
+  addItem() {
+    const newItem: ShoppingListItem = { id: "0", name: this.newItemName };
+    this.http.put(`${this.apiUrl}/shoppingList`, newItem).subscribe(
+      (response) => {
+        this.getShoppingItems()
+      }
+    );
+  }
+
+  public getShoppingItems() {
+    this.http.get<ShoppingListItem[]>(`${this.apiUrl}/shoppingList`).subscribe(
+      (response) => {
+        this.shoppingList = response
+      },
+      () => {
+
+      }
+    );
+  }
+
+
+
+  deleteItem(id: string) {
+
+  }
 }
