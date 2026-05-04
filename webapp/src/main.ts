d6d2d8e09dd7e5a40477735b7eb039e1c37ee04f
Seewald Maximilian <Maximilian-Seewald@gmx.net>
2026-05-05 01:26:30 +0200

    fix database for HA

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -3,7 +3,8 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { APP_BASE_HREF } from '@angular/common';
 
-const baseHref = window.location.pathname.startsWith('/app/') ? '/app/' : '/';
+const pathParts = window.location.pathname.split('/').filter(p => p);
+const baseHref = (pathParts[0] === 'app' && pathParts[1]) ? '/app/' + pathParts[1] + '/' : '/';
 const config = { ...appConfig, providers: [...(appConfig.providers || []), { provide: APP_BASE_HREF, useValue: baseHref }] };
 
 bootstrapApplication(AppComponent, config)
